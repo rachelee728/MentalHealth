9060d6ccd17188190021877c4a25c6625163b699
Arvind Anand <arvindanand1123@gmail.com>
2021-03-27 14:24:15 -0400

    Removing Frontend Code

--- dialog.js
+++
@@ -1,9 +0,0 @@
-document.forms[0].onsubmit = function(e) {
-    e.preventDefault(); // Prevent submission
-    var message = document.getElementById('pass').value;
-    document.getElementById("pass").innerHTML;
-    chrome.runtime.getBackgroundPage(function(bgWindow) {
-        bgWindow.setMsg(message);
-        window.close();     // Close dialog
-    });
-};
--- open-dialog.js
+++
@@ -1,2 +0,0 @@
-if (confirm('Write something nice about yourself! We promise it will help'))
-    chrome.runtime.sendMessage({type:'request_msg'});
